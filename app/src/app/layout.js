67262c0389bed861f0e7bf1783f627040f37a4cf
prajal-gautam <prajal.gautam.co@gmail.com>
2026-07-16 12:55:26 +0545

    Load toast notification styles

--- app/src/app/layout.js
+++ app/src/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import "react-toastify/dist/ReactToastify.css";
 import Header from "@/components/Header";
 import MainLayout from "@/layouts/MainLayout";
 import { ToastContainer } from "react-toastify";
